bc0a6358823dce91f20e9bc3ae7b7ff33af11d30
STRANDS Jenkins <strands@hanheide.net>
2015-05-17 17:01:38 +0000

    Revert "Trimmed the branch to only the topological_logging_manager sub directory"
    This reverts commit cacc8a9766397a86c1c4aaf33a9c3a61bbe3e018.

---
+++ topological_utils/scripts/load_yaml_map.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import json
+import yaml
+import sys
+
+from strands_navigation_msgs.msg import TopologicalNode
+import mongodb_store.util as dc_util
+from mongodb_store.message_store import MessageStoreProxy
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2 :
+        print "usage: insert_map input_file.txt"
+	sys.exit(2)
+
+    filename=str(sys.argv[1])
+
+    msg_store = MessageStoreProxy(collection='topological_maps')
+
+    print "loading %s"%filename
+    yaml_data=open(filename, "r")
+    
+    data = yaml.load(yaml_data)
+    
+    print "printing vlaa"
+
+    
+    for i in data:
+        meta = i['meta']
+        print i['node']
+        msgv = dc_util.dictionary_to_message(i['node'], TopologicalNode)
+        #print msgv, meta
+        msg_store.insert(msgv,meta)
+        #mongodb_store.util.store_message(points_db,p,val)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
