91a4dedd3860538f72546db43f08f67988f2bca0
STRANDS Jenkins <strands@hanheide.net>
2016-03-21 16:53:13 +0000

    Trimmed the branch to only the strands_navigation sub directory

--- monitored_navigation/src/monitored_navigation/__init__.py
+++
@@ -1,8 +0,0 @@
-import navigation
-import monitor_state
-import recover_state
-import recover_state_machine
-import ui_helper
-import help_manager
-
-__all__ = ['navigation', 'monitor_state', 'recover_state', 'recover_state_machine', 'ui_helper', 'help_manager']
